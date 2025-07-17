173f4cb5795aa2077330e9b7e73aafdd0a7c4201
Vasu Tyagi <vasutyagi014@gmail.com>
2025-07-18 00:19:28 +0530

    react components

--- App.js
+++ App.js
@@ -10,25 +10,47 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1", key: "child-1" }, [
-    React.createElement("h1", { key: "child-1-h1" }, "I'm an h1 tag"),
-    React.createElement("h2", { key: "child-1-h2" }, "I'm an h2 tag")
-  ]),
-  React.createElement("div", { id: "child2", key: "child-2" }, [
-    React.createElement("h1", { key: "child-2-h1" }, "I'm another h1 tag"),
-    React.createElement("h2", { key: "child-2-h2" }, "I'm another h2 tag")
-  ])
-]);
-//reacteleement is a object
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child1", key: "child-1" }, [
+//     React.createElement("h1", { key: "child-1-h1" }, "I'm an h1 tag"),
+//     React.createElement("h2", { key: "child-1-h2" }, "I'm an h2 tag")
+//   ]),
+//   React.createElement("div", { id: "child2", key: "child-2" }, [
+//     React.createElement("h1", { key: "child-2-h1" }, "I'm another h1 tag"),
+//     React.createElement("h2", { key: "child-2-h2" }, "I'm another h2 tag")
+//   ])
+// ]);
+// //reacteleement is a object
 
-// const heading=React.createElement("h1",{id:"heading"},"Hello world from React!");//it is an react element thats why it is object
-// first argument is what element we have to create 
-// second argument is Object where we give attribute to the eleemnt like id etc
-// third argument is what we have to write in that element
+// // const heading=React.createElement("h1",{id:"heading"},"Hello world from React!");//it is an react element thats why it is object
+// // first argument is what element we have to create 
+// // second argument is Object where we give attribute to the eleemnt like id etc
+// // third argument is what we have to write in that element
+
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// // we have created a root where dom manipulation and all works here and everything we will render inside this which is div we created named as root above
+
+
+// root.render(parent);
+
+
+//React element
+
+const heading = React.createElement("h1",{id:"heading"},"hello react");
+
+const jsheading=(
+<h1 className="head">
+  React using jsx
+  </h1>);//it is also an object as above
+//babel transform jsx into react element
+//for multiline jsx we are using round bracket
+
+//REACT COMPONENT 
+//REACT FUNCTIONAL COMPONENT 
+
+
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
-// we have created a root where dom manipulation and all works here and everything we will render inside this which is div we created named as root above
 
 
-root.render(parent);
+root.render(jsheading);
